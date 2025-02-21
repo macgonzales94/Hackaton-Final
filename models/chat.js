c14f67efbfe6415e0b199e0be8f25a8f70493245
macgonzales94 <ma.cgonzales.94@gmail.com>
2025-02-20 20:26:05 -0500

    bug arreglado de chat

--- models/chat.js
+++ models/chat.js
@@ -1,9 +1,9 @@
 const mongoose = require("mongoose");
 
-const ChatSchema = new mongoose.Schema({
+const chatSchema = new mongoose.Schema({
     usuario: { type: String, required: true },
     mensaje: { type: String, required: true },
     fecha: { type: Date, default: Date.now }
 });
 
-module.exports = mongoose.model("Chat", ChatSchema);
+module.exports = mongoose.model("chat", chatSchema);
